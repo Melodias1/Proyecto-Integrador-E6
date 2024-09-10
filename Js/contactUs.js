e07c3cd9d85cdfdef0c757045f6b681840e5ba27
Francisco <takincillo@hotmail.com>
2024-09-10 08:03:31 -0600

    se agrega funcion del btn de enviar, con las validaciones incluuidas, falta conectar al servicio de emails

--- Js/contactUs.js
+++ Js/contactUs.js
@@ -1,10 +1,14 @@
-const ENVIAR = document.getElementById ("button");
+const btnEnviar = document.getElementById ("btnEnviar");
 const alertValidaciones = document.getElementById("alertValidaciones");
 const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+const confirmValidaciones = document.getElementById("confirmValidaciones")
+const confirmValidacionesTexto = document.getElementById("confirmValidacionesTexto")
 const txtNombre = document.getElementById("Nombre");
-const txtCorreoElectronico = document.getElementById("Correo-electronico");
+const txtEmail = document.getElementById("Email");
 const txtTelefono = document.getElementById("Telefono");
 const txtComentarios = document.getElementById("Comentarios");
+
+
 
 
 let isValid = true;
@@ -26,10 +30,73 @@ function validarTelefono(){
     return true;
 }//* validarTelefono
 
+function isValidEmail() {
 
-if (! validarTelefono()){
-    txtTelefono.style.border= "solid red medium";
-    alertValidacionesTexto.innerHTML+="El <strong>Teléfono</strong> no es correcto.<br/>";
-    alertValidaciones.style.display="block";
-    isValid = false;
-}//* validarTelefono
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  
+    return regex.test(txtEmail.value);
+  }
+
+// boton enviar
+
+ btnEnviar.addEventListener("click",function(event){
+    event.preventDefault();
+
+        txtNombre.style.border="";
+        txtTelefono.style.border="";
+        txtEmail.style.border="";
+        alertValidacionesTexto.innerHTML=""
+        alertValidaciones.style.display="none"
+        confirmValidacionesTexto.innerHTML=""
+        confirmValidaciones.style.display="none"
+        isValid= true
+
+
+    if (!validarTelefono()){
+        txtTelefono.style.border= "solid red medium";
+        alertValidacionesTexto.innerHTML+="El <strong>Teléfono</strong> no es correcto.<br/>";
+        alertValidaciones.style.display="block";
+        isValid = false;
+    }//* validarTelefono
+
+    if (!isValidEmail()){
+        txtEmail.style.border= "solid red medium";
+        alertValidacionesTexto.innerHTML+=" El <strong>Correo Electronico</strong> no es valido.<br/>";
+        alertValidaciones.style.display="block";
+        isValid = false;
+    }//* validaremail
+   
+
+        if (txtNombre.value.length<3) {
+            txtNombre.style.border= "solid red medium";
+        alertValidacionesTexto.innerHTML+=" El <strong>Nombre</strong> no es Valido.<br/>";
+        alertValidaciones.style.display="block";
+        isValid = false;
+             
+        
+        }// nombreIsValid
+
+        if (txtComentarios.value.length<=0) {
+            txtComentarios.style.border= "solid red medium";
+        alertValidacionesTexto.innerHTML+=" Por favor,escriba su <strong>mensaje</strong>.<br/>";
+        alertValidaciones.style.display="block";
+        isValid = false;
+             
+        
+        }//isMensajevalid
+
+
+        if(isValid){
+            confirmValidaciones.innerHTML+=" El <strong>Registro</strong> fue exitoso, favor de revisar su correo electronico.<br/>";
+        confirmValidaciones.style.display="block";
+
+        
+        } // is valid
+
+ })
+
+ 
+
+
+
+
