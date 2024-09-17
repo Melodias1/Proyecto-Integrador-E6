54e2ae15ce8ef4da43b5f45dfbb6719e553447db
Francisco <takincillo@hotmail.com>
2024-09-17 09:03:32 -0600

    se centran las opciones de la nav bar

--- Js/navbar-footer.js
+++ Js/navbar-footer.js
@@ -31,8 +31,8 @@ const navBar=`<nav class="navbar navbar-expand-md bg-white ">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+        <div class="collapse navbar-collapse " id="navbarNav">
+            <ul class="navbar-nav m-auto">
 
             <li class="nav-item">
                 <a class="nav-link active" href="${linkHome}">${home}</a>
