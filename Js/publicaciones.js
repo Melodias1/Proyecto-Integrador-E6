1a73906ef30d3f34aeba26cc49f21d18a9e31640
Enrique Gutierrez <khike.gtz@gmail.com>
2024-09-19 21:51:15 -0600

    Se modifico la pagina de publicaciones

--- Js/publicaciones.js
+++ Js/publicaciones.js
@@ -92,4 +92,17 @@ addItem({'name':'Teriyaki Chicken Casserole',
 
 addItem({'name':'Duck Confit',
     'img':'https://www.themealdb.com/images/media/meals/wvpvsu1511786158.jpg',
-    'description':'french duck recipe'}) 
+    'description':'french duck recipe'})
+
+var myWidget = cloudinary.createUploadWidget({
+  cloudName: 'my_cloud_name', 
+  uploadPreset: 'my_preset'}, (error, result) => { 
+    if (!error && result && result.event === "success") { 
+      console.log('Done! Here is the image info: ', result.info); 
+    }
+  }
+)
+
+document.getElementById("upload_widget").addEventListener("click", function(){
+    myWidget.open();
+  }, false);
