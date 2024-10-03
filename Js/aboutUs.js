17a734932d93cf05be02021041b2e0baca23aab2
Alberto Herrera Nieto <alberto.herrera.dev@gmail.com>
2024-10-02 18:44:45 -0600

    Se creo el archivo para agregar la funcionalidad del boton

---
+++ Js/aboutUs.js
@@ -0,0 +1,5 @@
+let btnRegistro= document.getElementById("registrarse")
+
+btnRegistro.addEventListener("click", () => {
+    window.location.href = '../WebPages/RegistroUsuarios.html'; // Redirige a la misma pestaña
+});
