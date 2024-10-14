011403ac6b3759da12402f219cc06ca4494caa91
Ricardo A. Cervantes <ricardo.cervantes.ver@gmail.com>
2024-10-13 23:27:33 -0600

    Se agreg[o un mensaje de bienvenida

--- Js/feed .js
+++ Js/feed .js
@@ -50,6 +50,11 @@ function addItem(item) {
     <div class="col-sm-12">
         <div class="card mb-5 col-sm" style="max-width: 28em">
             <div class="card-body col-sm">
+            <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+</svg>
+            </div>
                 <h5 class="card-title">${item.userFirstName} ${item.userLastName}</h5>
             </div>
             <img src="${item.img}" class="card-img-top" alt="image" style="max-height:14em;">
@@ -60,7 +65,7 @@ function addItem(item) {
                 <div class="comments-section" id="comments-${item.date || Date.now()}">
                     <h6>Comentarios:</h6>
                     <div class="comments-list" style="max-height: 150px; overflow-y: auto;"></div>
-                    <textarea class="form-control" placeholder="Escribe un comentario..." rows="2" style="resize:none;"></textarea>
+                    <textarea class="form-control" placeholder="Escribe un comentario..." rows="2" style="resize:none"></textarea>
                     <a href="#" class="btn btn-primary mt-2" onclick="addComment(event,'${item.date || Date.now()}')">Hacer Comentario</a>
                     <button class="btn btn-danger mt-2" onclick="removeRecipe(event,'${item.name}')">Eliminar Publicación</button>
                 </div>
